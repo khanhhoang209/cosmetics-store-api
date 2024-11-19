05b7ff5de6adc24300280dcbd2aa701dc4cc3a54
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-11-19 18:18:13 +0700

    update appsettings.json and add DbContext to Progame.cs

--- CosmeticsStore.Controllers/Program.cs
+++ CosmeticsStore.Controllers/Program.cs
@@ -1,9 +1,20 @@
+using CosmeticsStore.Repositories.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<CosmeticsStoreDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("CosmeticsStore"));
+});
+
+
 
 var app = builder.Build();
 
