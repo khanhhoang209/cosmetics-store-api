cb77e44f8fab8441623d1c0c6b3e2db8f1c5e2bd
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-11-20 01:36:32 +0700

    add new migration

---
+++ CosmeticsStore.Repositories/Migrations/20241119180403_Modify_Order.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CosmeticsStore.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class Modify_Order : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Total",
+                table: "Order",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Total",
+                table: "Order",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
