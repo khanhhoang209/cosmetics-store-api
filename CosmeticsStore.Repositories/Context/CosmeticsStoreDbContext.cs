0822011779de708148a30fc0e0c4a3c8de4589d5
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-11-19 18:02:20 +0700

    add DbContext

---
+++ CosmeticsStore.Repositories/Context/CosmeticsStoreDbContext.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using CosmeticsStore.Repositories.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmeticsStore.Repositories.Context;
+
+public partial class CosmeticsStoreDbContext : DbContext
+{
+    public CosmeticsStoreDbContext()
+    {
+    }
+
+    public CosmeticsStoreDbContext(DbContextOptions<CosmeticsStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cart> Carts { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cart>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Cart");
+
+            entity.Property(e => e.UserId).HasMaxLength(50);
+
+            entity.HasOne(d => d.Product).WithMany()
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("C_FK_P");
+
+            entity.HasOne(d => d.User).WithMany()
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("C_FK_U");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Category__19093A0B692C8A1B");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.Name).HasMaxLength(200);
+            entity.Property(e => e.Status).HasDefaultValue(true);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__Order__C3905BCF8B9C06B4");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.OrderId).ValueGeneratedNever();
+            entity.Property(e => e.PhoneNumber).HasMaxLength(10);
+            entity.Property(e => e.ShippingStatus).HasMaxLength(200);
+            entity.Property(e => e.Total).HasColumnType("money");
+            entity.Property(e => e.UserId).HasMaxLength(50);
+
+            entity.HasOne(d => d.User).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("O_FK_U");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.HasKey(e => new { e.OrderId, e.ProductId }).HasName("OD_PK");
+
+            entity.ToTable("OrderDetail");
+
+            entity.Property(e => e.Price).HasColumnType("money");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("OD_FK_O");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("OD_FK_P");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Product__B40CC6CD3F67C549");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.Image).HasMaxLength(200);
+            entity.Property(e => e.Name).HasMaxLength(200);
+            entity.Property(e => e.Price).HasColumnType("money");
+            entity.Property(e => e.Status).HasDefaultValue(true);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("P_FK_C");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.RoleId).HasName("PK__Role__8AFACE1AA8B4E88C");
+
+            entity.ToTable("Role");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.NormalizedName).HasMaxLength(100);
+            entity.Property(e => e.Status).HasDefaultValue(true);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__User__1788CC4C7406F471");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.UserId).HasMaxLength(50);
+            entity.Property(e => e.Address).HasMaxLength(200);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FirstName).HasMaxLength(100);
+            entity.Property(e => e.LastName).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(50);
+            entity.Property(e => e.Status).HasDefaultValue(true);
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Users)
+                .HasForeignKey(d => d.RoleId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("U_FK_R");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
