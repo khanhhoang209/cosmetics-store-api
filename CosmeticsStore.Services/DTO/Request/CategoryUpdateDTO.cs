75d1238e1873892bcf2142e3d3550239fbfb7313
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-11-20 16:39:30 +0700

    add get category by name (paging), update, remove and restore api

---
+++ CosmeticsStore.Services/DTO/Request/CategoryUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CosmeticsStore.Services.DTO.Request;
+
+public class CategoryUpdateDTO
+{
+    [Required(ErrorMessage = "Vui lòng nhập ID!")]
+    public int CategoryId { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng nhập tên!")]
+    public string? Name { get; set; }
+
+    [Required]
+    public bool? Status { get; set; } = true;
+}
--- CosmeticsStore.Services/Interfaces/ICategoryService.cs
+++ CosmeticsStore.Services/Interfaces/ICategoryService.cs
@@ -1,11 +1,16 @@
-﻿using CosmeticsStore.Services.DTO.Request;
+﻿using CosmeticsStore.Repositories.Models.Domain;
+using CosmeticsStore.Services.DTO.Request;
+using CosmeticsStore.Services.DTO.Response;
 using CosmeticsStore.Services.Schema;
 
 namespace CosmeticsStore.Services.Interfaces;
 
 public interface ICategoryService
 {
+    Task<ServiceResponse> GetAllAsync(CategoryGetDTO category);
     Task<ServiceResponse> GetByIdAsync(int id);
     Task<ServiceResponse> CreateAsync(CategoryCreateDTO category);
-
+    Task<ServiceResponse> UpdateAsync(CategoryUpdateDTO category);
+    Task<ServiceResponse> RemoveAsync(int id);
+    Task<ServiceResponse> RestoreAsync(int id);
 }
