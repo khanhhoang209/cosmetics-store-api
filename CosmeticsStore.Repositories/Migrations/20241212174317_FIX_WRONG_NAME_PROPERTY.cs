1ad160a2b92f4372f5be2453b83322b577603a3e
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-12-13 01:42:38 +0700

    add new table

---
+++ CosmeticsStore.Repositories/Migrations/20241212174317_FIX_WRONG_NAME_PROPERTY.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CosmeticsStore.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class FIX_WRONG_NAME_PROPERTY : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpiryDate",
+                table: "RefreshToken",
+                newName: "ExpirationTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpirationTime",
+                table: "RefreshToken",
+                newName: "ExpiryDate");
+        }
+    }
+}
