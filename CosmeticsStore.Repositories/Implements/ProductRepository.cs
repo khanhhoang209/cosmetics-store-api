93dd5f5d56b02a8b2d02fa68e2e6bf724ecc72db
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-12-16 15:11:12 +0700

    added product repository

---
+++ CosmeticsStore.Repositories/Implements/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using CosmeticsStore.Repositories.Context;
+using CosmeticsStore.Repositories.Interfaces;
+using CosmeticsStore.Repositories.Models.Domain;
+
+namespace CosmeticsStore.Repositories.Implements;
+
+public class ProductRepository : GenericRepository<Product>, IProductRepository
+{
+    public ProductRepository(CosmeticsStoreDbContext dbContext) : base(dbContext)
+    {
+    }
+}
